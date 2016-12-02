1d12d054e2c525123272a771edcd35d1ef47035d
Woonsan Ko <woonsan@apache.org>
2016-12-02 15:48:21 -0500

    initial creation

---
+++ src/main/java/com/github/woonsan/SimpleOakAppApplication.java
@@ -0,0 +1,12 @@
+package com.github.woonsan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOakAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleOakAppApplication.class, args);
+	}
+}
