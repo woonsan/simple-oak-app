b00a4627c34538af03e61cb062f92a16bd296c3c
Woonsan Ko <woonsan@apache.org>
2016-12-02 16:33:33 -0500

    initial oak integration

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # simple-oak-app
 
 Simple Jackrabbit OAK based application for my own learning / testing.
+
+## How to run
+
+    $ mvn spring-boot:run
+
+## How to test
+
+    $ curl -v http://localhost:8080/greeting/
+    
+    $ curl -v --data "message=Hello+World" http://localhost:8080/greeting/
+    
+
--- src/main/java/com/github/woonsan/oak/app/simple/SimpleOakAppApplication.java
+++ src/main/java/com/github/woonsan/oak/app/simple/SimpleOakAppApplication.java
@@ -1,8 +1,18 @@
 package com.github.woonsan.oak.app.simple;
 
+import javax.jcr.Node;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +20,81 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class SimpleOakAppApplication {
 
+    private static Logger log = LoggerFactory.getLogger(SimpleOakAppApplication.class);
+
+    @Autowired
+    private Repository repository;
+
     public static void main(String[] args) {
         SpringApplication.run(SimpleOakAppApplication.class, args);
     }
 
     @RequestMapping("/echo")
     public String getEcho(@RequestParam(name = "message", required = false) String message) {
+        return message;
+    }
+
+    @RequestMapping(path = { "/greeting" }, method = { RequestMethod.GET })
+    public String getGreeting() {
+        String message = "";
+        Session session = null;
+
+        try {
+            session = repository.login(new SimpleCredentials("admin", "admin".toCharArray()));
+
+            Node root = session.getRootNode();
+            Node greeting = null;
+
+            if (root.hasNode("greeting")) {
+                greeting = root.getNode("greeting");
+
+                if (greeting.hasProperty("message")) {
+                    message = greeting.getProperty("message").getString();
+                }
+            }
+        } catch (RepositoryException e) {
+            log.error("Failed to read greeting message.", e);
+        } finally {
+            if (session != null) {
+                session.logout();
+            }
+        }
+
+        return message;
+    }
+
+    @RequestMapping(path = { "/greeting" }, method = { RequestMethod.POST })
+    public String createGreeting(@RequestParam(name = "message", required = true) String message) {
+        Session session = null;
+
+        try {
+            session = repository.login(new SimpleCredentials("admin", "admin".toCharArray()));
+
+            Node root = session.getRootNode();
+            Node greeting = null;
+
+            if (root.hasNode("greeting")) {
+                greeting = root.getNode("greeting");
+            } else {
+                greeting = root.addNode("greeting");
+            }
+
+            greeting.setProperty("message", message);
+            session.save();
+        } catch (RepositoryException e) {
+            log.error("Failed to save greeting message.", e);
+
+            try {
+                session.refresh(false);
+            } catch (RepositoryException re) {
+                log.error("Failed to refresh.", re);
+            }
+        } finally {
+            if (session != null) {
+                session.logout();
+            }
+        }
+
         return message;
     }
 }
