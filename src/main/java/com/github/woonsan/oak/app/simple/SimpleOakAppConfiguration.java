57535bb7a70e62958da2467f01415aea4ad6dfdd
Woonsan Ko <woonsan@apache.org>
2020-06-14 18:09:40 -0400

    use tar segment node store

--- src/main/java/com/github/woonsan/oak/app/simple/SimpleOakAppConfiguration.java
+++ src/main/java/com/github/woonsan/oak/app/simple/SimpleOakAppConfiguration.java
@@ -16,12 +16,20 @@
  */
 package com.github.woonsan.oak.app.simple;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.annotation.PreDestroy;
 import javax.jcr.Repository;
 
 import org.apache.jackrabbit.api.JackrabbitRepository;
 import org.apache.jackrabbit.oak.Oak;
 import org.apache.jackrabbit.oak.jcr.Jcr;
+import org.apache.jackrabbit.oak.segment.SegmentNodeStore;
+import org.apache.jackrabbit.oak.segment.SegmentNodeStoreBuilders;
+import org.apache.jackrabbit.oak.segment.file.FileStore;
+import org.apache.jackrabbit.oak.segment.file.FileStoreBuilder;
+import org.apache.jackrabbit.oak.segment.file.InvalidFileStoreVersionException;
 import org.apache.jackrabbit.server.remoting.davex.JcrRemotingServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,8 +57,17 @@ public class SimpleOakAppConfiguration {
 
     @Bean
     public Repository repository() {
-        repository = new Jcr(new Oak()).createRepository();
-        log.info("Reository creaetd: {}", repository);
+        try {
+            final FileStore fs = FileStoreBuilder.fileStoreBuilder(new File("repository")).build();
+            final SegmentNodeStore ns = SegmentNodeStoreBuilders.builder(fs).build();
+            repository = new Jcr(new Oak(ns)).createRepository();
+            log.info("Reository creaetd with nodeStore, {}", ns);
+        } catch (InvalidFileStoreVersionException e) {
+            log.error("Failed to create repository due to invalid file store version.", e);
+        } catch (IOException e) {
+            log.error("Failed to create repository due to IO exception.", e);
+        }
+
         return repository;
     }
 
